8251dc5d8772bec7315d4ba219a2cb833ac1a27f
jinlei01 <jinlei01@baidu.com>
2017-12-09 13:08:13 +0800

    传送门链接修改

--- footer_lite.php
+++ footer_lite.php
@@ -9,10 +9,27 @@
 <script src="https://cdn.bootcss.com/jquery/3.2.1/jquery.min.js"></script>
 
 
-<!-- Piwik Image Tracker-->
-<img src="http://localhost/piwik/piwik.php?idsite=1&rec=1&action_name=tongji" style="border:0" alt="" />
-<!-- End Piwik -->
+<!-- 百度统计 -->
+<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?8ab92ca2867f4c7be52c6ce61e2eb598";
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
 
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-108503769-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-108503769-1');
+</script>
 
 </body>
 </html>
