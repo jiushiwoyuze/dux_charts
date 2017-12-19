673a4210cbab9f138db1209a00d208867e5aebc7
jinlei01 <jinlei01@baidu.com>
2017-12-19 17:12:27 +0800

    图表库完全重构，从 PHP 改为 jekyll

---
+++ src/lite_bar/bar_h_base.php
@@ -0,0 +1,11 @@
+<?php
+    $page_title = '条形/柱状图表 度量衡图表演示库';
+?>
+
+
+<?php include '../php/header_lite.php'; ?>
+
+<div id="bar_h_base"></div>
+
+
+<?php include '../php/footer_lite.php'; ?>
---
+++ src/php/header_lite.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="zh">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php print $page_title; ?></title>
+    <meta name="description" content="百度商业设计规范库 度量衡图表 示例库">
+    <meta name="author" content="ThirtyJin">
+
+
+    <link rel="stylesheet" type="text/css" href="../assets/css/style.css">
+
+
+    <!-- 引入 echarts.js -->
+    <script type="text/javascript" src="../assets/js/echarts.min.js"></script>
+    <script src="../assets/js/dux-theme.js"></script>
+
+
+<!-- Piwik -->
+<script type="text/javascript">
+  var _paq = _paq || [];
+  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="//youcanyouup.info/piwik/";
+    _paq.push(['setTrackerUrl', u+'piwik.php']);
+    _paq.push(['setSiteId', '1']);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+  })();
+</script>
+<!-- End Piwik Code -->
+
+</head>
+<body style="background: #ffffff; margin: 0;">
